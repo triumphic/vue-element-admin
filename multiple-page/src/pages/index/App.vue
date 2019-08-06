0ae4c44a50e558e06a3ecfd37ac96a414f40c586
likai <likai@hubcloud.com.cn>
2019-08-06 10:30:29 +0800

    add multiplePaage

---
+++ multiple-page/src/pages/index/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        index页面{{msg}}
+        <router-view></router-view>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            msg: 'index'
+        }
+    }
+}
+</script>
